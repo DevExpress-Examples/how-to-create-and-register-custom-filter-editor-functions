c9f7ffc2afb7ac6d8daa053838acfab069aaf66f
CORP\tochilkin.alexander <13348208+AlexMedusa@users.noreply.github.com>
2019-09-26 19:52:32 +0300

    Implement the ICustomFunctionDisplayAttributes interface

--- VB/DXApplication5/CustomFunctions/IsWeekend.vb
+++ VB/DXApplication5/CustomFunctions/IsWeekend.vb
@@ -1,11 +1,4 @@
 ﻿Imports DevExpress.Data.Filtering
-Imports DevExpress.Utils.Svg
-Imports System
-Imports System.Collections.Generic
-Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 
 Namespace DXSample
 	Public Class IsWeekendFunction
@@ -20,54 +13,54 @@ Namespace DXSample
 		Public Shared Function Unregister() As Boolean
 			Return CriteriaOperator.UnregisterCustomFunction(instance)
 		End Function
-		Public ReadOnly Property Category() As FunctionCategory
-			Get
-				Return FunctionCategory.DateTime
-			End Get
-		End Property
-		Public ReadOnly Property Description() As String
-			Get
-				Return "TEST TEST>"
-			End Get
-		End Property
+        Public ReadOnly Property Category As FunctionCategory Implements ICustomFunctionOperatorBrowsable.Category
+            Get
+                Return FunctionCategory.DateTime
+            End Get
+        End Property
+        Public ReadOnly Property Description As String Implements ICustomFunctionOperatorBrowsable.Description
+            Get
+                Return "TEST TEST>"
+            End Get
+        End Property
 
-		Public Function IsValidOperandCount(ByVal count As Integer) As Boolean
-			Return count = 1
-		End Function
-		Public Function IsValidOperandType(ByVal operandIndex As Integer, ByVal operandCount As Integer, ByVal type As Type) As Boolean
-			Return type Is GetType(Date)
-		End Function
-		Public ReadOnly Property MaxOperandCount() As Integer
-			Get
-				Return 1
-			End Get
-		End Property
-		Public ReadOnly Property MinOperandCount() As Integer
-			Get
-				Return 1
-			End Get
-		End Property
-		Public Function Evaluate(ParamArray ByVal operands() As Object) As Object
-			Dim dt As Date = Convert.ToDateTime(operands(0))
-			Return dt.DayOfWeek = DayOfWeek.Sunday OrElse dt.DayOfWeek = DayOfWeek.Saturday
-		End Function
-		Public ReadOnly Property Name() As String
-			Get
-				Return FunctionName
-			End Get
-		End Property
-		Public ReadOnly Property DisplayName() As String
-			Get
-				Return "Is weekend"
-			End Get
-		End Property
-		Public ReadOnly Property Image() As Object
-			Get
-				Return "DayView;Office2013"
-			End Get
-		End Property
-		Public Function ResultType(ParamArray ByVal operands() As Type) As Type
-			Return GetType(Boolean)
-		End Function
-	End Class
+        Public Function IsValidOperandCount(count As Integer) As Boolean Implements ICustomFunctionOperatorBrowsable.IsValidOperandCount
+            Return count = 1
+        End Function
+        Public Function IsValidOperandType(operandIndex As Integer, operandCount As Integer, type As Type) As Boolean Implements ICustomFunctionOperatorBrowsable.IsValidOperandType
+            Return type Is GetType(Date)
+        End Function
+        Public ReadOnly Property MaxOperandCount As Integer Implements ICustomFunctionOperatorBrowsable.MaxOperandCount
+            Get
+                Return 1
+            End Get
+        End Property
+        Public ReadOnly Property MinOperandCount As Integer Implements ICustomFunctionOperatorBrowsable.MinOperandCount
+            Get
+                Return 1
+            End Get
+        End Property
+        Public Function Evaluate(ParamArray operands() As Object) As Object Implements ICustomFunctionOperator.Evaluate
+            Dim dt As Date = Convert.ToDateTime(operands(0))
+            Return dt.DayOfWeek = DayOfWeek.Sunday OrElse dt.DayOfWeek = DayOfWeek.Saturday
+        End Function
+        Public ReadOnly Property Name As String Implements ICustomFunctionOperator.Name
+            Get
+                Return FunctionName
+            End Get
+        End Property
+        Public ReadOnly Property DisplayName As String Implements ICustomFunctionDisplayAttributes.DisplayName
+            Get
+                Return "Is weekend"
+            End Get
+        End Property
+        Public ReadOnly Property Image As Object Implements ICustomFunctionDisplayAttributes.Image
+            Get
+                Return "DayView;Office2013"
+            End Get
+        End Property
+        Public Function ResultType(ParamArray operands() As Type) As Type Implements ICustomFunctionOperator.ResultType
+            Return GetType(Boolean)
+        End Function
+    End Class
 End Namespace
--- VB/DXApplication5/CustomFunctions/NotBeginsWith.vb
+++ VB/DXApplication5/CustomFunctions/NotBeginsWith.vb
@@ -1,9 +1,4 @@
 ﻿Imports DevExpress.Data.Filtering
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 
 Namespace DXSample.CustomFunctions
 	Public Class NotBeginsWithFunction
@@ -17,57 +12,57 @@ Namespace DXSample.CustomFunctions
 		Public Shared Function Unregister() As Boolean
 			Return CriteriaOperator.UnregisterCustomFunction(instance)
 		End Function
-		Public ReadOnly Property Category() As FunctionCategory
-			Get
-				Return FunctionCategory.Text
-			End Get
-		End Property
-		Public ReadOnly Property Description() As String
-			Get
-				Return "<description>"
-			End Get
-		End Property
-		Public Function IsValidOperandCount(ByVal count As Integer) As Boolean
-			Return count = 2
-		End Function
-		Public Function IsValidOperandType(ByVal operandIndex As Integer, ByVal operandCount As Integer, ByVal type As Type) As Boolean
-			Return type Is GetType(String)
-		End Function
-		Public ReadOnly Property MaxOperandCount() As Integer
-			Get
-				Return 2
-			End Get
-		End Property
-		Public ReadOnly Property MinOperandCount() As Integer
-			Get
-				Return 2
-			End Get
-		End Property
-		Public Function Evaluate(ParamArray ByVal operands() As Object) As Object
-			If operands(0) IsNot Nothing AndAlso operands(1) IsNot Nothing Then
-				Dim str1 As String = operands(0).ToString()
-				Dim str2 As String = operands(1).ToString()
-				Return Not str1.StartsWith(str2, StringComparison.InvariantCultureIgnoreCase)
-			End If
-			Return False
-		End Function
-		Public ReadOnly Property Name() As String
-			Get
-				Return FunctionName
-			End Get
-		End Property
-		Public ReadOnly Property DisplayName() As String
-			Get
-				Return "Not begins with"
-			End Get
-		End Property
-		Public ReadOnly Property Image() As Object
-			Get
-				Return "FontSizeDecrease;Office2013"
-			End Get
-		End Property
-		Public Function ResultType(ParamArray ByVal operands() As Type) As Type
-			Return GetType(Boolean)
-		End Function
-	End Class
+        Public ReadOnly Property Category As FunctionCategory Implements ICustomFunctionOperatorBrowsable.Category
+            Get
+                Return FunctionCategory.Text
+            End Get
+        End Property
+        Public ReadOnly Property Description As String Implements ICustomFunctionOperatorBrowsable.Description
+            Get
+                Return "<description>"
+            End Get
+        End Property
+        Public Function IsValidOperandCount(count As Integer) As Boolean Implements ICustomFunctionOperatorBrowsable.IsValidOperandCount
+            Return count = 2
+        End Function
+        Public Function IsValidOperandType(operandIndex As Integer, operandCount As Integer, type As Type) As Boolean Implements ICustomFunctionOperatorBrowsable.IsValidOperandType
+            Return type Is GetType(String)
+        End Function
+        Public ReadOnly Property MaxOperandCount As Integer Implements ICustomFunctionOperatorBrowsable.MaxOperandCount
+            Get
+                Return 2
+            End Get
+        End Property
+        Public ReadOnly Property MinOperandCount As Integer Implements ICustomFunctionOperatorBrowsable.MinOperandCount
+            Get
+                Return 2
+            End Get
+        End Property
+        Public Function Evaluate(ParamArray operands() As Object) As Object Implements ICustomFunctionOperator.Evaluate
+            If operands(0) IsNot Nothing AndAlso operands(1) IsNot Nothing Then
+                Dim str1 As String = operands(0).ToString()
+                Dim str2 As String = operands(1).ToString()
+                Return Not str1.StartsWith(str2, StringComparison.InvariantCultureIgnoreCase)
+            End If
+            Return False
+        End Function
+        Public ReadOnly Property Name As String Implements ICustomFunctionOperator.Name
+            Get
+                Return FunctionName
+            End Get
+        End Property
+        Public ReadOnly Property DisplayName As String Implements ICustomFunctionDisplayAttributes.DisplayName
+            Get
+                Return "Not begins with"
+            End Get
+        End Property
+        Public ReadOnly Property Image As Object Implements ICustomFunctionDisplayAttributes.Image
+            Get
+                Return "FontSizeDecrease;Office2013"
+            End Get
+        End Property
+        Public Function ResultType(ParamArray operands() As Type) As Type Implements ICustomFunctionOperator.ResultType
+            Return GetType(Boolean)
+        End Function
+    End Class
 End Namespace
